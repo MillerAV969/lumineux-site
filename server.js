9b4f839b7185e20653f9602b96dd79cf8d4a9dec
MillerAV969 <miller96929@gmail.com>
2025-10-29 23:42:38 +0300

    Добавил основные файлы сайта

--- server.js
+++ server.js
@@ -2,20 +2,17 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files
 app.use(express.static(__dirname));
 
-// Main route
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Catch all
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
